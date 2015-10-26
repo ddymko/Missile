2e495d7d3267d1b7ec2ef40f9d4ab1a58bc12e19
ddymko <dymkod@gmail.com>
2015-10-26 19:24:34 -0400

    Modified deploy method and using symbols instead of string

--- missile.rb
+++ missile.rb
@@ -4,7 +4,7 @@ require 'yaml'
 require 'net/scp'
 
 
-class Test < Thor
+class Missile < Thor
 
   desc 'setup PROJECT_NAME', 'creates a config file for a specific deployment'
   option :d, :type => :boolean
@@ -23,24 +23,20 @@ class Test < Thor
       web_path = STDIN.gets.chomp
       puts 'Please enter local path to project'
       local_path = STDIN.gets.chomp
-      configs = {'host' => host, 'username' => username, 'password' => password, 'web_path' => web_path, 'local_path' => local_path}
-      path = Dir.pwd
-      project =  Dir.pwd + "/configs/" + project + '.yaml'
-      
+      configs = {:host => host, :username => username, :password => password, :web_path => web_path, :local_path => local_path}
+      project =  Dir.pwd + '/configs/' + project + '.yaml'
       File.open(File.expand_path(project), 'w+') { |file| file.write(configs.to_yaml) }
     end
   end
 
   desc 'deploy PROJECT_NAME', 'this will start a deplyoment for the specifiied project'
-
   def deploy(project)
 
-    project = project + '.yaml'
-    config = YAML.load_file(project)
-
-    Net::SCP.upload!(config['host'], config['username'],
-                     config['local_path'], config['web_path'],
-                     :ssh => {:password => config['password']}, :recursive => true)
+    project =  '/configs/' + project + '.yaml'
+    config = YAML.load_file(File.join(__dir__, project ))
+    Net::SCP.upload!(config[:host], config[:username],
+                     config[:local_path], config[:web_path],
+                     :ssh => {:password => config[:password]}, :recursive => true)
   end
 end
-Test.start(ARGV)
+Missile.start(ARGV)
