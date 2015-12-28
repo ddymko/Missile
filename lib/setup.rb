5961d6cf8656edacb46f15d0189589fc0f3e2902
ddymko <dymkod@gmail.com>
2015-12-28 18:20:21 -0500

    finished up the setup calls

--- lib/setup.rb
+++ lib/setup.rb
@@ -6,4 +6,21 @@ class Setup
     puts response
   end
 
+  def self.create(project_dir)
+
+    print 'Please enter your host > '
+    host = STDIN.gets.chomp
+    print 'Please enter your username > '
+    username = STDIN.gets.chomp
+    print 'Please enter your password > '
+    password = STDIN.gets.chomp
+    print 'Please enter web path to project > '
+    web_path = STDIN.gets.chomp
+    print 'Please enter local path to project > '
+    local_path = STDIN.gets.chomp
+
+    configs = {:host => host, :username => username, :password => password, :web_path => web_path, :local_path => local_path}
+    File.open(File.expand_path(project_dir), 'w+') { |file| file.write(configs.to_yaml) }
+  end
+
 end
--- missile.rb
+++ missile.rb
@@ -12,24 +12,8 @@ class Missile < Thor
   desc 'setup [project_name]', 'creates a config file for a specific deployment'
   option :d, :type => :boolean , :desc => "Deletes specified project"
   def setup(project)
-
     project_dir =  @@file_path + '/' + project + '.yaml'
-    if options[:d]
-      Setup.delete(project_dir)
-    else
-      print 'Please enter your host > '
-      host = STDIN.gets.chomp
-      print 'Please enter your username > '
-      username = STDIN.gets.chomp
-      print 'Please enter your password > '
-      password = STDIN.gets.chomp
-      print 'Please enter web path to project > '
-      web_path = STDIN.gets.chomp
-      print 'Please enter local path to project > '
-      local_path = STDIN.gets.chomp
-      configs = {:host => host, :username => username, :password => password, :web_path => web_path, :local_path => local_path}
-      File.open(File.expand_path(project_dir), 'w+') { |file| file.write(configs.to_yaml) }
-    end
+    options[:d] ? Setup.delete(project_dir) : Setup.create(project_dir)
   end
 
   ## List actions
