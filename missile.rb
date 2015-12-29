bd9f3b5aa4ac131349a749adf975b823ea2f82c8
ddymko <dymkod@gmail.com>
2015-12-28 19:48:38 -0500

    keeping project_dir dry

--- missile.rb
+++ missile.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
+APP_ROOT = File.dirname(__FILE__)
 require 'thor'
 require 'yaml'
 require 'net/scp'
-require_relative 'lib/list'
-require_relative 'lib/setup'
-require_relative 'lib/deploy'
+require File.join(APP_ROOT, 'lib', 'list')
+require File.join(APP_ROOT, 'lib', 'setup')
+require File.join(APP_ROOT, 'lib', 'deploy')
+
 
 class Missile < Thor
 
@@ -13,22 +15,25 @@ class Missile < Thor
   desc 'setup [project_name]', 'creates a config file for a specific deployment'
   option :d, :type => :boolean , :desc => "Deletes specified project"
   def setup(project)
-    project_dir =  @@file_path + '/' + project + '.yaml'
-    options[:d] ? Setup.delete(project_dir) : Setup.create(project_dir)
+    options[:d] ? Setup.delete(project_path(project)) : Setup.create(project_path(project))
   end
 
   ## List actions
   desc 'list', 'Displays available deployments'
   option :i, :type => :boolean , :desc => "[project name] -i Displays information about specific deployment setup "
   def list(project="")
-    project = @@file_path + "/" + project + ".yaml"
-    options[:i] ?  List.info(project) : List.list_all(@@file_path)
+    options[:i] ?  List.info(project_path(project)) : List.list_all(@@file_path)
   end
 
   desc 'deploy PROJECT_NAME', 'this will start a deployment for the specified project'
   def deploy(project)
-    project_dir =  @@file_path + '/' + project + '.yaml'
-    Deploy.deploy(project_dir)
+    Deploy.deploy(project_path(project))
   end
+
+  no_commands{
+    def project_path(project)
+      return project_dir =  @@file_path + '/' + project + '.yaml'
+    end
+  }
 end
 Missile.start(ARGV)
